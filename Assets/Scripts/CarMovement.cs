96db9779e28abc2899709fb41ad5be59b84268b3
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-11-25 18:16:29 -0600

    animated car movement

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -7,20 +7,39 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     private Vector3 targetPosition;
-    private float speed = 3.0f;
+    private float moveDuration = 0.5f;
+    private bool isMoving = false;
 
     public void moveTowardsPosition(Vector3 newPos)
     {
-        targetPosition = newPos;
+        if (!isMoving)
+        {
+            targetPosition = newPos;
+            StartCoroutine(MoveToPosition(targetPosition, moveDuration));
+        }
+    }
+
+    // Corutina para mover el objeto
+    private IEnumerator MoveToPosition(Vector3 target, float duration)
+    {
+        isMoving = true;
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, target, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+        isMoving = false;
     }
 
     void Start()
     {
+        // Inicializar con la posición actual para evitar movimientos no deseados
         targetPosition = transform.position;
-    }
-
-    void Update()
-    {
-        transform.position = targetPosition;
     }
 }
