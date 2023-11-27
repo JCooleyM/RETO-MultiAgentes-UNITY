223249a8e52be1c70d6850404e5b49d5f1aa395e
Jorge Cooley Magallanes <a00833271@tec.mx>
2023-11-27 01:08:12 -0600

    Creacion de escena

--- Assets/Scripts/TrenMovimiento.cs
+++ Assets/Scripts/TrenMovimiento.cs
@@ -6,14 +6,17 @@ public class TrenMovimiento : MonoBehaviour
 {
     float speed = 25f;
     Vector3 origen = new Vector3(0, 0, 0);
-    Vector3 destino = new Vector3(0, 0, 65);
+    Vector3 destino = new Vector3(0, 0, 70);
+    Vector3 position = new Vector3(0, -0.14f, 30);
+    private Quaternion rotation = Quaternion.Euler(0, 90, 0);
+    public GameObject Vias;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Vias, position, rotation);
     }
 
     // Update is called once per frame
