a9ee81c957b931b23cabe4f3b4d4d749f887a1df
Jorge Cooley Magallanes <a00833271@tec.mx>
2023-11-27 13:15:53 -0600

    Ya solo faltan los semaforos

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -8,6 +8,7 @@ public class CarMovement : MonoBehaviour
 {
     private Vector3 targetPosition;
     private float moveDuration = 0.5f;
+    private float rotationDuration = .5f;
     private bool isMoving = false;
 
     public void moveTowardsPosition(Vector3 newPos)
@@ -39,7 +40,38 @@ public class CarMovement : MonoBehaviour
 
     void Start()
     {
-        // Inicializar con la posición actual para evitar movimientos no deseados
+        // Inicializar con la posiciï¿½n actual para evitar movimientos no deseados
         targetPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verificar si el objeto colisionado tiene la etiqueta "Rotator"
+        if (other.CompareTag("SalidaTunel"))
+        {
+            // Rotar 90 grados en el eje Y
+            transform.Rotate(0, -90, 0);
+        }
+
+        else if (other.CompareTag("EntradaAvenida"))
+        {
+            StartCoroutine(RotacionGradual(90, rotationDuration));
+        }
+    }
+
+     private IEnumerator RotacionGradual(float angulo, float duracion)
+    {
+        Quaternion iniciarRotacion = transform.rotation;
+        Quaternion finRotacion = Quaternion.Euler(0, angulo, 0) * iniciarRotacion;
+        float tiempo = 0;
+
+        while (tiempo < duracion)
+        {
+            transform.rotation = Quaternion.Slerp(iniciarRotacion, finRotacion, tiempo / duracion);
+            tiempo += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = finRotacion;
     }
 }
